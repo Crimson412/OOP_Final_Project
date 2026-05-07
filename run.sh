ee72873c5480be6fb690f291ca9ad676b4adac54
Crimson <lorenzo.marez412@gmail.com>
2026-05-07 16:22:45 -0600

    updated run.sh to work wtihout lib or out directories made

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-rm -rf out/* lib/lib.jar
+mkdir -p lib/
+rm -rf out/* lib/*
 javac src/oop/model/* src/oop/service/* -d out/
 jar cf ./lib/lib.jar -C out .
 javac -cp "lib/lib.jar:." src/oop/main/* -d out/
